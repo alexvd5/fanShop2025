dbfdb331086a454c8791f05410fc4847c675b1c6
a_yordanovv <aleksandar_yordanov2001@abv.bg>
2025-03-31 19:32:21 +0300

    News Controller and View Added

---
+++ fanShop2025/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fanShop2025.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
